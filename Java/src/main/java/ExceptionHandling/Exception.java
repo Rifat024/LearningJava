d41fe734d1c07855f601a9bb4b8022ac604a66a2
rifatbinsiraj <rifatrabbi024@gmail.com>
2022-04-06 23:32:06 +0600

    EXCEPTION

--- Java/src/main/java/ExceptionHandling/Exception.java
+++ Java/src/main/java/ExceptionHandling/Exception.java
@@ -3,17 +3,28 @@ package ExceptionHandling;
 public class Exception  {
     public static void main(String[] args) {
 
-try {
-    int a = 10;
-    int b = 0;
-    int result=a / b;
-    System.out.println(result);
-}catch (ArithmeticException e) {
-    System.out.println(e);
-}
-finally {
-    System.out.println("last line");
-}
+       /* try {
+            int a = 10;
+            int b = 0;
+            int result=a / b;
+            System.out.println(result);
+        }catch (ArithmeticException e) {
+            System.out.println(e);
+        }
+        finally {
+            System.out.println("last line");
+        }*/
 
+        try {
+            int result = Integer.parseInt("Rifat");
+            System.out.println(result);
+        }catch (NumberFormatException nfe) {
+            System.out.println("Exception= "+nfe);
+        }
+        finally {
+            System.out.println("last line");
+        }
+        //  int number = Integer.parseInt("pants");
     }
+
 }
