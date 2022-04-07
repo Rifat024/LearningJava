230b755b3a8efcfe6d211dabc4ba5ae3e43b4c24
Rifat Bin Siraj <rifatrabbi024@gmail.com>
2022-04-07 21:02:19 +0600

    Multi Threads

--- Java/src/main/java/ThreadsAndMultiThreads/MultiThreadMain.java
+++ Java/src/main/java/ThreadsAndMultiThreads/MultiThreadMain.java
@@ -1,11 +1,12 @@
 package ThreadsAndMultiThreads;
 
 public class MultiThreadMain {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
        for(int i=0;i<5;i++){
            MultiThreads multiThreads=new MultiThreads(i);
            multiThreads.start();
-           multiThreads.isAlive();
+           System.out.println(i+" no thread is alive? Answer= "+multiThreads.isAlive());
+           Thread.sleep(10000);
        }
 
     }
